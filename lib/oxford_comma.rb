df000f4aca154f5a4af50243ec4e95c5a96dea9b
Erik Weir <erik.weir@key.me>
2020-06-03 19:31:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,14 @@ def oxford_comma(array)
   case array.length
   when 1 
     "#{array[0]}"
+<<<<<<< HEAD
   when 2 
       array[0..1].join(" and ")
   else array[0...-1].join(", ") << ", and #{array[-1]}"
+=======
+    when 2 
+      array[0..2].join(", and #{array[-1]}")
+    else array[0...3].join(", ") << ", and #{array[-1]}"
+>>>>>>> 5fafedf46b3d9f5b9c39441120799cac402ec311
   end
 end
