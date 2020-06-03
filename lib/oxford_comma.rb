071dea1e16216e49daed8789d0a7c6190c0174f5
Erik Weir <erik.weir@key.me>
2020-06-03 19:32:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,5 @@ def oxford_comma(array)
   when 2 
       array[0..1].join(" and ")
   else array[0...-1].join(", ") << ", and #{array[-1]}"
-
-    when 2 
-      array[0..2].join(", and #{array[-1]}")
-    else array[0...3].join(", ") << ", and #{array[-1]}"
   end
 end
